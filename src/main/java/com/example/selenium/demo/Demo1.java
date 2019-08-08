4c13ad1f154bd11b511006d2e29c60d43e876f30
paolo-ibm123 <53495015+paolo-ibm123@users.noreply.github.com>
2019-08-09 04:05:36 +0800

    Add files via upload

---
+++ src/main/java/com/example/selenium/demo/Demo1.java
@@ -0,0 +1,50 @@
+package com.example.selenium.demo;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Demo1 {
+	
+	private static WebDriver driver;
+	private static WebDriverWait wait;
+
+	public Demo1() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		File file = new File(classLoader.getResource("test.html").getFile());
+		String site = file.getAbsolutePath();
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\ProgramData\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, 20);
+		
+		driver.get(site);
+		
+		try {
+			WebElement element = driver.findElement(By.id("date"));
+			String type = element.getAttribute("type");
+			System.out.println("[type]" + type);
+		} catch (NoSuchElementException nse) {
+			System.out.println("Error: type not found!");
+		}
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {			
+			e.printStackTrace();
+		}
+		
+		driver.close();
+		driver.quit();
+	}
+
+}
---
+++ src/main/java/com/example/selenium/demo/Demo2.java
@@ -0,0 +1,48 @@
+package com.example.selenium.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Demo2 {
+	
+	private static WebDriver driver;
+	private static WebDriverWait wait;
+
+	public Demo2() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		String site = "file:///C:/tools/test.html";
+		
+		System.setProperty("webdriver.ie.driver", "C:\\ProgramData\\Drivers\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		wait = new WebDriverWait(driver, 20);
+		
+		driver.get(site);
+		
+		try {
+			WebElement element = driver.findElement(By.id("date"));
+			String type = element.getAttribute("type");
+			System.out.println("[type]" + type);
+		} catch (NoSuchElementException nse) {
+			System.out.println("Error: type not found!");
+		}
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {			
+			e.printStackTrace();
+		}
+		
+		
+		driver.close();
+		driver.quit();
+	}
+
+}
